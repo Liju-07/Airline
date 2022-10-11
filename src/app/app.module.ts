0f3cbc60df15acc01815219aff32ba6c89311224
Liju-07 <lijusaju03@gmail.com>
2022-10-11 18:12:50 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPassComponent } from './view-pass/view-pass.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewAirlineComponent } from './view-airline/view-airline.component';
+import { RouterModule,Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:ViewAirlineComponent
+  },
+  {
+    path:"airline",component:ViewAirlineComponent
+  },
+  {
+    path:"pass",component:ViewPassComponent
+  },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPassComponent,
+    NavbarComponent,
+    ViewAirlineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
